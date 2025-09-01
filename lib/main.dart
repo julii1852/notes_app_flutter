3d32b826b6e9bf1984f8c26d6f159f485cd67a79
julii1852 <85981772+julii1852@users.noreply.github.com>
2025-09-01 13:07:36 +0400

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'screens/notes_list_screen.dart';
+
+void main() {
+  runApp(NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Notes App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: NotesListScreen(),
+    );
+  }
+}
